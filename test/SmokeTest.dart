ec3bb5cb41a4d024de6ea944ef0a0bc84737bc40
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-02-23 03:15:52 +0700

    a

---
+++ test/SmokeTest.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:bai1/main.dart';
+
+void main() {
+  testWidgets('smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+  });
+}
